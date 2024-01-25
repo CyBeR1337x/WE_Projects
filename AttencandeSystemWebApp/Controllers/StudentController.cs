6732d9ec20fb5e279e10d399adec3afb3beac9aa
Syed Shah Meer Haider <60551634+CyBeR1337x@users.noreply.github.com>
2024-01-25 22:00:40 +0500

    ....

---
+++ AttencandeSystemWebApp/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AttencandeSystemWebApp.Controllers {
+	public class StudentController : Controller {
+		StuCrsDBEntities db = new StuCrsDBEntities();
+		// GET: Student
+		public ActionResult MainView() {
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult AddStudent(StudentCourse stuCrs) {
+			db.StudentCourses.Add(stuCrs);
+			db.SaveChanges();
+			return RedirectToAction("MainView");
+		}
+		[HttpGet]
+		public ActionResult AttendanceView(string id) {
+			return View(db.StudentCourses.Where(i => i.subject == id).ToList());
+		}
+
+		[HttpPost]
+		public ActionResult AttendanceView(IEnumerable<StudentCourse> ls) {
+			foreach (var item in ls)
+				db.StudentAttendances.Add(new StudentAttendance { date = DateTime.Now, present = item.isPresent, rollno = item.rollno, subject = item.subject });
+
+			db.SaveChanges();
+			return RedirectToAction("MainView");
+		}
+
+
+	}
+}
---
+++ AttencandeSystemWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AttencandeSystemWebApp.MvcApplication" Language="C#" %>
